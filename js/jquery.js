15af5fd7082165cc43bc30860e2fbff473b4676b
DOUBLEPOR <doublepor.pp$gmail.com>
2020-04-19 19:28:39 +0700

    add graphs

--- js/jquery.js
+++ js/jquery.js
@@ -14,38 +14,44 @@ $(function () {
         selectedColor: "#FF4040",
         showTooltip: true,
         onRegionClick: function (element, code, region) {
-            $.getJSON(url, function (result) {
-                var selectedCountry = result[region];
-                console.log(selectedCountry);
-                if(selectedCountry != null){
-                    var total = selectedCountry.length;
-                    console.log(total);
-                }
-            });
+        $.getJSON(url, function (result) {
+            var selectedCountry = result[region];
+            console.log(selectedCountry);
+            if (selectedCountry != null) {
+            var total = selectedCountry.length;
+            console.log(total);
+            }
+        });
         },
-    });
+    })    
 
-    jQuery("#vvmap").vectorMap({
-        map: "world_en",
-        backgroundColor: "#3333FF",
-        borderColor: "#ffffff",
-        borderOpacity: 2,
-        borderWidth: 1,
-        color: "#3333FF",
-        enableZoom: true,
-        hoverColor: "#EECFA1",
-        normalizeFunction: "linear",
-        selectedColor: "#FF4040",
-        showTooltip: true,
-        onRegionClick: function (element, code, region) {
-            $.getJSON(url, function (result) {
-                var selectedCountry = result[region];
-                console.log(selectedCountry);
-                if(selectedCountry != null){
-                    var total = selectedCountry.length;
-                    console.log(total);
-                }
-            });
+    var chartData = {
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
+        datasets: [{
+            data: [589, 445, 483, 503, 689, 692, 634],
         },
-    });
+        {
+            data: [639, 465, 493, 478, 589, 632, 674],
+        }]
+    };
+
+    var chLine = document.getElementById("chLine");
+    if (chLine) {
+        new Chart(chLine, {
+        type: 'line',
+        data: chartData,
+        options: {
+            scales: {
+            yAxes: [{
+                ticks: {
+                beginAtZero: false
+                }
+            }]
+            },
+            legend: {
+            display: false
+            }
+        }
+        });
+    }
 });
